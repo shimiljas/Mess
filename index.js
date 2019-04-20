11ac8ccafc081f3dfaca8dccdf01bce3916fc44e
shimil <iamshimil@gmail.com>
2019-04-20 20:00:30 +0530

    master

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ import App from './app/App';
 import { name as appName } from './app.json';
 import Test from './test';
 
-AppRegistry.registerComponent(appName, () => Test);
+AppRegistry.registerComponent(appName, () => App);
--- test.js
+++ test.js
@@ -15,66 +15,7 @@ class Test extends React.Component {
 
           justifyContent: 'center'
         }}
-      >
-        <MapView
-          // remove if not using Google Maps
-          style={{ flex: 1 }}
-          onPress={mkr => console.warn(mkr.nativeEvent.coordinate)}
-          region={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.015,
-            longitudeDelta: 0.0121
-          }}
-        >
-          <GooglePlacesAutocomplete
-            placeholder="Search"
-            minLength={2}
-            autoFocus={true}
-            returnKeyType={'search'}
-            listViewDisplayed={false}
-            fetchDetails={true}
-            onPress={(data, details = null) => {
-              console.log(details.geometry.location);
-              // 'details' is provided when fetchDetails = true
-            }}
-            query={{
-              key: 'AIzaSyA5m8U4SYKBhV4IldQs409Jv28L3avWYtM',
-              language: 'en'
-            }}
-            nearbyPlacesAPI="GooglePlacesSearch"
-            debounce={200}
-            styles={{
-              textInputContainer: {
-                backgroundColor: 'rgba(0,0,0,0)',
-                borderTopWidth: 0,
-                borderBottomWidth: 0
-              },
-              textInput: {
-                marginLeft: 10,
-                marginRight: 10,
-
-                height: 60,
-                backgroundColor: 'white',
-                color: '#5d5d5d',
-                fontSize: 16
-              },
-              predefinedPlacesDescription: {
-                color: '#1faadb'
-              }
-            }}
-          />
-        </MapView>
-        <View
-          style={{
-            width: '100%',
-            height: 120,
-            position: 'absolute',
-            backgroundColor: 'red',
-            bottom: 0
-          }}
-        />
-      </View>
+      />
     );
   }
 }
